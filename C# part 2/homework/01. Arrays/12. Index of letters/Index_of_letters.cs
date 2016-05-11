9226167bfe0a9984ad8910bd59524d9e0473b744
peturganchev <peturganchev93@gmail.com>
2016-05-11 12:14:07 +0300

    problem 12 solved problem 2 index to arrays.sln fixed

---
+++ C# part 2/homework/01. Arrays/12. Index of letters/Index_of_letters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Index_of_letters
+{
+    static void Main()
+    {
+        char[] alphabet = new char[26];
+        string word = Console.ReadLine().ToUpper();
+        char[] wordToChar = word.ToCharArray();
+        int index = 0;
+        for (int i = 0; i < alphabet.Length; i++)
+        {
+            alphabet[i] = (char)(i + 65);
+        }
+        for (int i = 0; i < wordToChar.Length; i++)
+        {
+            for (int j = 0; j < alphabet.Length; j++)
+            {
+                if (wordToChar[i] == alphabet[j])
+                {
+                    index = j;
+                    break;
+                }
+            }
+            Console.WriteLine(index);
+        }
+    }
+}
