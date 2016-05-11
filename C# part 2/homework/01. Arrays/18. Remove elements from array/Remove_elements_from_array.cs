c312a556578517ccf909a31670ebfe5e45c0e550
peturganchev <peturganchev93@gmail.com>
2016-05-11 15:58:38 +0300

    problem 18 solved

---
+++ C# part 2/homework/01. Arrays/18. Remove elements from array/Remove_elements_from_array.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Remove_elements_from_array
+    {
+        static void Main()
+        {
+        int length = int.Parse(Console.ReadLine());
+        int[] array = new int[length];
+        int[] LIS = new int[length];
+        int result;
+        for (int i = 0; i < length; i++)
+        {
+            array[i]= int.Parse(Console.ReadLine());
+            LIS[i] = 1;
+        }
+        for (int i = 1; i < array.Length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (array[i] >= array[j] & LIS[i]<LIS[j]+1)
+                {
+                    LIS[i] = LIS[j] + 1;
+                }
+            }
+        }
+        
+        result = length - LIS.Max();
+       Console.WriteLine(result);
+        }
+    }
